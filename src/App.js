494fc16fd9c77f69fd92475972e30084950656ff
LightningYTlol <unconfigured@null.spigotmc.org>
2022-08-18 16:59:50 -0700

    Redirects added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import { Route , Link, Redirect } from 'react-router-dom'
+
+
+
+function App() {
+        return (
+            <div className="App">
+                <Route exact path="/invite" component={() => {
+                    window.location.href = 'https://discord.com/api/oauth2/authorize?client_id=1004287257412370482&permissions=8&scope=bot%20applications.commands';
+                    return null;
+                }}/>
+            </div>
+)
+
+
+}
+
+export default App;
